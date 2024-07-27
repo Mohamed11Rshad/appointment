11d224484fcde4d7a68a2052817400a58cbb2752
Mohamed Rshad <126785911+Mohamed11Rshad@users.noreply.github.com>
2024-07-27 15:19:36 +0300

    - addad (development / production )flavors for android - changed android app name for each flavor - addad launch.json for flavors config - addad two main files to each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,11 @@ import 'package:appointment/core/di/dependency_injection.dart';
 import 'package:appointment/core/routing/app_router.dart';
 import 'package:appointment/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetTt();
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DocApp(
       appRouter: AppRouter(),
---
+++ lib/main_production.dart
@@ -0,0 +1,15 @@
+import 'package:appointment/core/di/dependency_injection.dart';
+import 'package:appointment/core/routing/app_router.dart';
+import 'package:appointment/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  setupGetTt();
+                                                                                                                                                                                                                                                                                                                                        await ScreenUtil.ensureScreenSize();
+  runApp(
+    DocApp(
+      appRouter: AppRouter(),
+    ),
+  );
+}
